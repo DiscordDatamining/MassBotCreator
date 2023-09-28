b4cf4e0ec8d22fd77f13e2ef40f35df741c78e0b
= <irismickens@gmail.com>
2023-09-28 14:49:00 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,21 @@
+from datetime import datetime
+
 import aiohttp
 import httpx
 import requests
-from datetime import datetime
-from geopy.geocoders import Nominatim
+from aiohttp import (
+    ClientConnectionError,
+    ClientConnectorCertificateError,
+    ClientConnectorError,
+    ClientConnectorSSLError,
+    ClientError,
+    ClientHttpProxyError,
+    ClientOSError,
+    ClientResponse,
+    ClientSession,
+)
 from colored import fg
+from geopy.geocoders import Nominatim
 from requests import (
     ConnectionError,
     JSONDecodeError,
@@ -21,3 +33,8 @@ class Client:
         self.hours = now.strftime("%I")
         self.minutes = now.strftime("%M")
         self.period = now.strftime("%p").upper()
+        self.green = fg("#ACD8A7")
+        self.red = fg("#FAA61A")
+        self.yellow = fg("#ffa500")
+        self.grey = fg("#2b2d31")
+        self.session: ClientSession = aiohttp.ClientSession
